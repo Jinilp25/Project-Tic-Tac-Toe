cadc1616408bfa7013c0bcea28bf880e6cb5ebf8
Jinilp25 <j.psg626@gmail.com>
2024-08-26 23:26:19 -0600

    Ver 1.2 Update

--- main.rb
+++ main.rb
@@ -1,4 +1,14 @@
 require_relative "lib/game.rb"
 
-tic_tac_toe = Game.new("Player 1", "Player 2")
+print "Player 1 enter your user name: "
+player1_name = gets.chomp
+puts "Player 1 name set to #{player1_name}"
+
+print "Player 2 enter your user name: "
+player2_name = gets.chomp
+puts "Player 2 name set to #{player2_name}"
+
+puts "\n#{player1_name} vs #{player2_name}!!\n\n"
+
+tic_tac_toe = Game.new(player1_name, player2_name)
 tic_tac_toe.play
